e9feb2a5e42f0bc613edc7030e0c257355b7a976
gaoTianLiang <gaoTianLiang>
2020-04-21 14:52:56 +0800

    String注释修改

--- src/java/lang/String.java
+++ src/java/lang/String.java
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 1994, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 package java.lang;
 
 import java.io.ObjectStreamField;
@@ -107,18 +82,11 @@ import java.util.regex.PatternSyntaxException;
  * @see     java.nio.charset.Charset
  * @since   JDK1.0
  */
-
-public final class String
-    implements java.io.Serializable, Comparable<String>, CharSequence {
+public final class String implements java.io.Serializable, Comparable<String>, CharSequence {
     /** The value is used for character storage. */
     private final char value[];
-
     /** Cache the hash code for the string */
     private int hash; // Default to 0
-
-    /** use serialVersionUID from JDK 1.0.2 for interoperability */
-    private static final long serialVersionUID = -6849794470754667710L;
-
     /**
      * Class String is special cased within the Serialization Stream Protocol.
      *
@@ -126,42 +94,8 @@ public final class String
      * <a href="{@docRoot}/../platform/serialization/spec/output.html">
      * Object Serialization Specification, Section 6.2, "Stream Elements"</a>
      */
-    private static final ObjectStreamField[] serialPersistentFields =
-        new ObjectStreamField[0];
+    private static final ObjectStreamField[] serialPersistentFields = new ObjectStreamField[0];
 
-    /**
-     * Initializes a newly created {@code String} object so that it represents
-     * an empty character sequence.  Note that use of this constructor is
-     * unnecessary since Strings are immutable.
-     */
-    public String() {
-        this.value = "".value;
-    }
-
-    /**
-     * Initializes a newly created {@code String} object so that it represents
-     * the same sequence of characters as the argument; in other words, the
-     * newly created string is a copy of the argument string. Unless an
-     * explicit copy of {@code original} is needed, use of this constructor is
-     * unnecessary since Strings are immutable.
-     *
-     * @param  original
-     *         A {@code String}
-     */
-    public String(String original) {
-        this.value = original.value;
-        this.hash = original.hash;
-    }
-
-    /**
-     * Allocates a new {@code String} so that it represents the sequence of
-     * characters currently contained in the character array argument. The
-     * contents of the character array are copied; subsequent modification of
-     * the character array does not affect the newly created string.
-     *
-     * @param  value
-     *         The initial value of the string
-     */
     public String(char value[]) {
         this.value = Arrays.copyOf(value, value.length);
     }
